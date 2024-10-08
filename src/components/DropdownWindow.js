6cbc596aad25d2ece56c8a0a1a4033e50a0b1991
harpreetAalda <harpreet.singh@aalda.in>
2024-10-08 14:31:15 +0530

    Update DropdownWindow.js

--- src/components/DropdownWindow.js
+++ src/components/DropdownWindow.js
@@ -2,20 +2,22 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 const DropdownWindow = ({layoutStyle, children}) => {
-  return <View style={{...styles.dropdownOverlayView, ...styles.shadow, ...layoutStyle}}>{children}</View>;
+  return <View style={{...styles.dropdownOverlayView, ...styles.shadow, ...layoutStyle,
+    // backgroundColor: 'white'
+  }}>{children}</View>;
 };
 
 export default DropdownWindow;
 
 const styles = StyleSheet.create({
   dropdownOverlayView: {
-    backgroundColor: '#EFEFEF',
+    // backgroundColor: '#EFEFEF',
   },
   shadow: {
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 6},
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-    elevation: 10,
+    // shadowColor: '#000',
+    // shadowOffset: {width: 0, height: 6},
+    // shadowOpacity: 0.1,
+    // shadowRadius: 10,
+    // elevation: 10,
   },
 });
